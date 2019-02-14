0c41b4d11856a1a341321f5949bdcf614ad15964
Pablo Gutierrez <pablo.gutierrezdiez@kbc.be>
2019-02-14 10:23:28 +0100

    Updated the full requirements for Beerstyle

--- beermenuAPI/src/models/beerstyle.model.ts
+++ beermenuAPI/src/models/beerstyle.model.ts
@@ -20,6 +20,61 @@ export class Beerstyle extends Entity {
   })
   styleNumber: string;
 
+  @property({
+    type: 'number',
+  })
+  category: number;
+
+  @property({
+    type: 'number',
+  })
+  ABVmin?: number;
+
+  @property({
+    type: 'number',
+  })
+  ABVmax?: number;
+
+  @property({
+    type: 'number',
+  })
+  OGmin?: number;
+
+  @property({
+    type: 'number',
+  })
+  OGmax?: number;
+
+  @property({
+    type: 'number',
+  })
+  FGmin?: number;
+
+  @property({
+    type: 'number',
+  })
+  FGmax?: number;
+
+  @property({
+    type: 'number',
+  })
+  IBUmin?: number;
+
+  @property({
+    type: 'number',
+  })
+  IBUmax?: number;
+
+  @property({
+    type: 'number',
+  })
+  SRMmin?: number;
+
+  @property({
+    type: 'number',
+  })
+  SRMmax?: number;
+
   constructor(data?: Partial<Beerstyle>) {
     super(data);
   }
